19d4954a35597ac5a2a55aac8f7687f3132a5d97
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-03-07 14:58:35 +0100

    Show first version

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
-@section('title')
+@section('title',$project->name)
 
 @section('content')
-
+<header>{{$project->name}}</header>
+<img src="{{$project->image}}" alt="$project->name">
+<p>{{$project->description}}</p>
 @endsection
